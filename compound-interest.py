a20f6f2300e57fcd9382a968839e1f208ad24b34
philippsedov <philippsedov@gmail.com>
2021-05-25 09:04:14 +0300

    Update compound-interest.py

--- compound-interest.py
+++ compound-interest.py
@@ -29,8 +29,8 @@ km = a
 for n in range(1, t+1):
     p = st + km * ( (1+(j-i)/m)**(m*n)-1 ) * m/(j-i)
     if n==1 or n%5==0:
-        print(int(p), n, y+n)
+        print(int(p), n)
     elif p<goal:
         print(int(p))
     else:
-        print(int(p), n, y+n)
+        print(int(p), n)
