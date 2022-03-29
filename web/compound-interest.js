773204bf8adc51f172b3e663e3b8ab2a5b908086
philippsedov <philippsedov@gmail.com>
2022-03-29 14:09:01 +0300

    исправил формулу расчёта
    не поделил проценты на 100

--- web/compound-interest.js
+++ web/compound-interest.js
@@ -8,11 +8,10 @@ function onInput () {
   var goal = Number(document.getElementById("goal").value);
   var nMonth = (Number(document.getElementById("nMonth").value))/12;
   var startCapital = Number(document.getElementById("startCapital").value);
-  var annualInterest = Number(document.getElementById("annualInterest").value);
-  var inflation = (Number(document.getElementById("inflation").value))+0.00000001;
-  var growth = ((annualInterest - inflation )/ 12);
-  //console.log(nYear);
-  setResult( Math.ceil(( (goal - startCapital) * growth *  (1/( (1+growth) ** (12 * nMonth)-1)) ) * 100)/100 );
+  var annualInterest = (Number(document.getElementById("annualInterest").value))/100;
+  var inflation = (Number(document.getElementById("inflation").value))/100+0.00000001;
+
+  setResult( Math.ceil(( (goal - startCapital) * ((annualInterest-inflation)/12) * (1/( (1+((annualInterest-inflation)/12))**(12*nMonth)-1 ) ))));
 }
 
 document.addEventListener("DOMContentLoaded", function () {
